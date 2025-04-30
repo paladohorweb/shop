39b3d45062eedd058632b63aece9b1020c5d0296
paladohorweb <paladohor@gmail.com>
2025-04-30 13:02:26 -0500

    change port

--- src/main/java/jgm/tiendaVirtual/TiendaVirtualApplication.java
+++ src/main/java/jgm/tiendaVirtual/TiendaVirtualApplication.java
@@ -1,5 +1,7 @@
 package jgm.tiendaVirtual;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,9 @@ public class TiendaVirtualApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaVirtualApplication.class, args);
+                
+                Map<String, Object> props = new HashMap<>();
+		props.put("server port", 8082);
 	}
 
 }
